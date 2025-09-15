976ca56c37b2f1531ec5b4bc255be739f2f156a5
Denton Manders <dentonmanders@gmail.com>
2025-09-14 21:05:23 -0700

    0.1

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/updays-app/' // replace with your GitHub repo name
+  homepage: 'https://codedenton.github.io/updays-app/', // replace with your GitHub repo name
+  "scripts": {
+  "start": "react-scripts start",
+  "build": "react-scripts build",
+  "predeploy": "npm run build",
+  "deploy": "gh-pages -d build"
+}
 
 })
