077f0b1e59675c1ac7534ff6288ab8e54343008a
Denton Manders <dentonmanders@gmail.com>
2025-09-14 20:57:57 -0700

    0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, HashRouter } from "react-router-dom";
 import Home from "./Home";
 import BadHabits from "./BadHabits";
 import './App.css'; // We'll define styles here
@@ -7,14 +7,14 @@ import GoodHabits from "./GoodHabits";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <NavBar/>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/good-habits" element={<GoodHabits />} />
             <Route path="/bad-habits" element={<BadHabits />} />
           </Routes> 
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
